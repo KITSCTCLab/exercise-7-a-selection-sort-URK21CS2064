3e3c50cbc960efba3fd9c4c02ef141c8d7105f14
URK21CS2064 <109648000+URK21CS2064@users.noreply.github.com>
2022-09-14 10:44:31 +0530

    completed

--- Main.py
+++ Main.py
@@ -3,11 +3,16 @@ from typing import List
 def selectionSort(array, size) -> List[int]:
   # Write your code here
   for j in range(size-1):
-    min =j
+    min =arr[j]
+    minpos=j
     for i in range(1,size):
-      if array[min]>array[i]:
-        min =i
-      swap(array[min],array[j]
+      if array[j]<min:
+        min =array[i]
+        minpos=j
+    temp=array[i]
+    array[i]=min
+    array[minpos]=temp
+  return array 
 
 # Do not change the following code
 in_data = input()
